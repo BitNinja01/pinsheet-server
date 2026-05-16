b23bb76ffe54f520c91e5bafd67864d786f537f1
BitNinja01 <bitninja01@pm.me>
2026-05-16 11:48:01 -0700

    pass full user environment to Chrome subprocess for system cursor theme

--- source/main.py
+++ source/main.py
@@ -736,7 +736,7 @@ def main():
     chrome = _find_chrome()
     chrome_proc = None
     if chrome:
-        chrome_proc = subprocess.Popen([chrome, f"--app={url}", "--start-maximized"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        chrome_proc = subprocess.Popen([chrome, f"--app={url}", "--start-maximized"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, env=os.environ.copy())
     else:
         webbrowser.open(url)
 
